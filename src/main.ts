169d7dd5f969e1fee58bd04ac499844987d467a6
CarlosOzon <carlos@ozon.mobi>
2023-08-05 04:16:48 -0500

    fix port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(80);
+  await app.listen(process.env.PORT || 80);
 }
 bootstrap();
